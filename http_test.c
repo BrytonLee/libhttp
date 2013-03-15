3babe2bc6aadad2a242abd6f49f579306fde327f
Bryton Lee <brytonlee01@gmail.com>
2013-03-15 08:20:15 +0800

    创建一定数量的进程处理请求，socket文件描述符的传递通过unix socket进行。 目前的代码是一个中间结果还没有完成。

--- http_test.c
+++ http_test.c
@@ -4,6 +4,7 @@
 #include <sys/types.h> 
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <sys/epoll.h>
 #include <signal.h>
 #include <unistd.h>
 #include "libhttp/mempool.h"
@@ -86,15 +87,174 @@ static void request_process(int sockfd)
 	mem_pool_entry_put(entry);
 }
 
+static int send_to_worker(int sfd, int sockfd)
+{
+	struct msghdr msg;
+	struct iovec iov[1];
+
+#ifdef HAVE_MSGHDR_MSG_CONTROL
+	union {
+		struct cmsghdr cm;
+		char control[CMSG_SPACE(sizeof(int))];
+	} control_un;
+	struct cmsghdr *cmptr;
+
+	if ( sockfd < 0 || sfd < 0 )
+		return -1;
+
+	msg.msg_control = control_un.control;
+	msg.msg_controllen = sizeof(control_un.control);
+
+	cmptr = CMSG_FIRSTHDR(&msg);
+	cmptr->cmsg_len = CMSG_LEN(sizeof(int));
+	cmptr->cmsg_level = SOL_SOCKET; /* UNIX domain */
+	cmptr->cmsg_type = SCM_RIGHTS; /* send/recvive file descriptor */
+	*((int *)CMSG_DATA(cmptr)) == sockfd;
+#else
+	if ( sockfd < 0 || sfd < 0 )
+		return -1;
+
+	msg.msg_accrights = (caddr_t) &sockfd;
+	msg.msg_accrightslen = sizeof(int);
+#endif
+	
+	msg.msg_name = NULL;
+	msg.msg_namelen = 0;
+	
+	//?? 发送0字节数据，不知道能否发送成功
+	iov[0].iov_base = NULL;
+	iov[0].iov_len = 0;
+	msg.msg_iov = iov;
+	msg.msg_iovlen = 1;
+
+	return (sendmsg(sfd, &msg, 0));
+}
+
+static int recv_from_master(int rfd)
+{
+	struct msghdr msg;
+	struct iovec iov[1];
+	ssize_t n;
+	int sockfd;
+
+#ifdef HAVE_MSGHDR_MSG_CONTROL
+	union {
+		struct cmsghdr cm;
+		char control[CMSG_SPACE(sizeof(int))];
+	}control_un;
+	struct cmsghdr *cmptr;
+
+	msg.msg_control = control_un.control;
+	msg.msg_controllen = sizeof(control_un.control);
+#else
+	int newfd;
+	msg.msg_accrights = (caddr_t) &newfd;
+	msg.msg_accrightslen = sizeof(int);
+#endif
+
+	if ( rfd < 0 )
+		return -1;
+	
+	msg.msg_name = NULL;
+	msg.msg_namelen = 0;
+
+	iov[0].iov_base = NULL;
+	iov[0].iov_len = 0;
+	msg.msg_iov = iov;
+	msg.msg_iovlen = 1;
+
+	if ( ( n = recvmsg(rfd, &msg, 0)) <= 0)
+		return n;
+
+#ifdef HAVE_MSGHDR_MSG_CONTROL
+	if (( cmptr = CMSG_FIRSTHDR(&msg)) != NULL &&
+			cmptr->cmsg_len == CMSG_LEN(sizeof(int))) {
+		if (cmptr->cmsg_level != SOL_SOCKET ||
+				cmptr->cmsg_type != SCM_RIGHTS)
+			return -1;
+		sockfd = *((int *)CMSG_DATA(cmptr));
+	} else
+		sockfd = -1;
+#else
+	if ( msg.msg_accrightslen == sizeof(int))
+		sockfd = newfd;
+	else
+		sockfd = -1;
+#endif
+	return sockfd;
+}
+
+/* worker 进程的工作函数，一直工作不退出 */
+static void worker_process(int sv)
+{
+	int sockfd, epfd, nfds;
+	struct epoll_event ev, evs[20];
+	int i, ret;
+
+	if ( sv < 0 ) {
+		printf("worker_process parameter error!\n");
+		return;
+	}
+
+	epfd = epoll_create(256);
+	if ( epfd < 0 ) {
+		perror("epoll_create error:");
+		return;
+	}
+	
+	ev.data.fd = sv;
+	ev.events = EPOLLIN|EPOLLET;
+	while ( (ret = epoll_ctl(epfd, EPOLL_CTL_ADD, sv, &ev) ) < 0 )
+		if ( errno == EINTR )
+			continue;
+		else {
+			perror("epoll_ctl error:");
+			return;
+		}
+	while ( 1 ) {
+		while ( ( nfds = epoll_wait(epfd, evs, 20, 500) ) < 0 ) {
+			if ( errno == EINTR )
+				continue;
+			else {
+				/* 此处如何容错??? */
+				perror("epoll_wait error:");
+			}
+		}
+
+		for (i = 0; i < nfds; i++) {
+			if( evs[i].data.fd == sv ) {
+				if ( (sockfd = recv_from_master(ev)) < 0 )
+					continue;
+				while ( ( ret = epoll_ctl(sockfd, EPOLL_CTL_ADD, sv, &ev) ) < 0 )
+					if ( errno == EINTR )
+						continue;
+					else 
+						perror("epoll_ctl error:");
+			} else if ( evs[i].events & EPOLLIN) {
+				request_process(evs[i].data.fd);
+			} /* else if ( evs[i].events & EPOLLOUT ) {
+				 TODO write data back to socket 
+			} */
+		}
+
+		/* TODO: 从epfd中删除sockfd的动作没做完 */
+	}
+}
+
 int main(int argc, char ** argv)
 {
 	int listenfd, sockfd;
 	struct sockaddr_in serv_addr;
 	struct sockaddr client_addr;
 	int addr_len;
+	int worker=1;
+	pid_t pid;
+	int	sv[2];
 	int ret = -1;
 	
 	signal(SIGUSR1, mem_dump);
+	signal(SIGCHLD, SIG_IGN);
+
 	listenfd = socket(AF_INET, SOCK_STREAM, 0);
 	if ( -1 == listenfd ) {
 		perror("Cannot create listen socket: ");
@@ -118,6 +278,29 @@ int main(int argc, char ** argv)
 		return ret;
 	}
 
+	ret = socketpair(AF_LOCAL, SOCK_STREAM, 0, sv);
+	if ( -1 == ret ) {
+		perror("socketpair error: ");
+		return ret;
+	}
+
+	/* TODO: 取得CPU的核数 */
+	worker = 4;
+	do { 
+		pid = fork();
+		if ( pid == 0 ) {
+			close(listenfd);
+			close(sv[1]);
+			/* 永远不返回 */
+			worker_process(sv[0]);
+		} else if ( pid < 0 ) {
+			perror("fork error: ");
+			return ret;
+		}
+	} while ( worker-- );
+
+	close(sv[0]);
+
 	do {
 		memset(&client_addr, '\0', addr_len);
 		sockfd = accept(listenfd, (struct sockaddr *)&client_addr, &addr_len);
@@ -128,7 +311,9 @@ int main(int argc, char ** argv)
 			return ret;
 		}
 
-		request_process(sockfd);
+		ret = send_to_worker(sv[1], sockfd);
+		if ( -1 == ret )
+			printf("send socket fd to worker error!\n");
 
 		while ( 1 ) {
 			ret = close(sockfd);
